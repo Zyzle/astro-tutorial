4add99dcc51180a5df7f8d7475c7920afcdfbbed
Colin McCulloch <colin.mcculloch@whocanfixmycar.com>
2025-08-25 02:40:26 +0100

    add preact component and theme button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://animated-manatee-85c44e.netlify.app/",
+  integrations: [preact()],
 });
